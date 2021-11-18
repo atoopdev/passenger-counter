661d0773960c36f1300ed22102bb8ae33f095091
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-11-18 12:57:49 -0800

    setup files and first exercises

---
+++ script.js
@@ -0,0 +1,13 @@
+// replaces text in h2 with id count-el to 5 from 0
+// document.getElementById("count-el").innerText = 5;
+
+//counter for tracking passengers
+let count = 0;
+
+console.log("Contents of count var: ", count);
+
+let myAge = 40;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);
