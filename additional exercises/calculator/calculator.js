d04b6b79d1694da2bf6d32afe121f14fff1314af
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-11-30 09:43:23 -0800

    added text description to calc output

--- additional exercises/calculator/calculator.js
+++ additional exercises/calculator/calculator.js
@@ -1,5 +1,6 @@
 let num1 = 8;
 let num2 = 2;
+let result;
 
 document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
@@ -8,20 +9,24 @@ calcOutput = document.getElementById("sum-el");
 
 function add(){
     console.log("add button clicked: num1 + num2 = ", num1 + num2);
-    calcOutput.textContent = num1 + num2;
+    result = num1 + num2;
+    calcOutput.textContent = "Sum: " + result;
 }
 
 function subtract(){
     console.log("subtract button clicked: num1 - num2 = ", num1 - num2);
-    calcOutput.textContent = num1 - num2;
+    result = num1 - num2;
+    calcOutput.textContent = "Subtract: " + result;
 }
 
 function divide(){
     console.log("divide button clicked: num1 / num2 = ", num1 / num2);
-    calcOutput.textContent = num1 / num2;
+    result = num1 / num2;
+    calcOutput.textContent = "Divide: " + result;
 }
 
 function multiply(){
     console.log("multiply button clicked: num1 * num2 = ", num1 * num2);
-    calcOutput.textContent = num1 * num2;
+    result = num1 * num2;
+    calcOutput.textContent = "Multiply: " + result;
 }
